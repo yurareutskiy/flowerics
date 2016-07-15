d57cf3bbf06d199ddb6e0f37d898c43acaee570c
SumLare <mrhard95@gmail.com>
2016-07-16 02:23:43 +0300

    Only get requests in api for flowers

--- routes/api/flowers.js
+++ routes/api/flowers.js
@@ -9,39 +9,11 @@ router.route("/")
       }
       res.json(flowers);
     });
-  })
-  .post(function (req, res, next) {
-    var flower = req.body;
-    Flower.createFlower(flower, function(err, flower) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(flower);
-    });
   });
 
 router.route('/:id')
   .get(function (req, res, next) {
     Flower.getFlowerById(req.params.id, function(err, flower) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(flower);
-    });
-  })
-  .put(function (req, res, next) {
-    var id = req.params.id;
-    var flower = req.body;
-    Flower.updateFlower(id, flower, {}, function(err, flower) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(flower);
-    });
-  })
-  .delete(function (req, res, next) {
-    var id = req.params.id;
-    Flower.removeFlower(id, function(err, flower) {
       if (err) {
         res.send(err);
       }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 
 router.use('/flowers', require('./flowers'));
-router.use('/users', require('./users'));
+
 
 module.exports = router;
