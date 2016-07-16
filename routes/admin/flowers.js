7c26d6263b8ddd8476dc2bd3e88fde1f1b01fe20
SumLare <mrhard95@gmail.com>
2016-07-17 00:09:27 +0300

    Image upload on create in flowers

--- routes/admin/flowers.js
+++ routes/admin/flowers.js
@@ -1,5 +1,21 @@
 var router = require('express').Router(),
-    Flower = require('../../models/flower');
+    Flower = require('../../models/flower'),
+    multer  = require('multer'),
+    crypto  = require('crypto'),
+    mime  = require('mime');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '../../../public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
+    });
+  }
+});
+
+var upload = multer({ storage: storage });
 
 router.route('/')
   .get(function (req, res, next) {
@@ -12,9 +28,14 @@ router.route('/')
       });
     });
   })
-  .post(function (req, res, next) {
-    var flower = req.body;
-    Flower.createFlower(flower, function(err, flower) {
+  .post(upload.single('image'), function (req, res, next) {
+    var flower = new Flower({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.file.filename
+    });
+    flower.save(function(err, flower) {
       if (err) {
         res.send(err);
       }
@@ -60,8 +81,13 @@ router.route('/:id/edit')
   })
   .put(function (req, res, next) {
     var id = req.params.id;
-    var flower = req.body;
-    Flower.updateFlower(id, flower, {}, function(err, flower) {
+    var flower = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.file.filename
+    };
+    flower.updateFlower(id, flower, {}, function(err, flower) {
       if (err) {
         res.send(err);
       }
