0cac246f0c2363711f3bd5134c9c14fa158c8c87
SumLare <mrhard95@gmail.com>
2016-08-04 20:59:28 +0300

    Get ready for production

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-"use strict";
+'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var less = require('less-middleware');
-var mongoose = require('mongoose');
-var MongoStore = require('connect-mongo')(session);
-var db = require('./config/db');
+/**
+ * Module dependencies
+ */
 
-var app = express();
+require('dotenv').config();
+
+const join = require('path').join;
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const config = require('./config');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const less = require('less-middleware');
+const MongoStore = require('connect-mongo')(session);
+const db = require('./config/db');
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 
 // set up session
 app.use(session({
--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'mongodb://localhost/flowerics_prduction'
+  db: 'mongodb://localhost/flowerics_production'
 };
