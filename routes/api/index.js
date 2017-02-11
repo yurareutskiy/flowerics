ae449b7641da8ab8991408662b056ecd8efa1320
SumLare <mrhard95@gmail.com>
2017-02-11 22:46:14 +0300

    API: Add new routes to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,5 +6,8 @@ router.use('/moods', require('./moods'));
 router.use('/promotions', require('./promotions'));
 router.use('/orders', require('./orders'));
 router.use('/users', require('./users'));
+router.use('/replies', require('./replies'));
+router.use('/promocodes', require('./promocodes'));
+router.use('/cards', require('./cards'));
 
 module.exports = router;
